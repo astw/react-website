e81573ba972712cf8d28f76b3aca5ec6f5644820
Shuhao Wang <wshuhao@gmail.com>
2017-09-10 13:04:01 -0700

    add header

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,11 +1,12 @@
 "use strict";
 
 var React = require('react'); 
+var Header = require("./common/header"); 
 
 var Home = React.createClass({
 	render: function(){
-		return (
-			<div className='jumbotron'>
+		return ( 
+			<div className='jumbotron'> 
 				<h1>Pluralsight administration</h1>
 				<p> React, React Router, and Flux for extra-responsive web apps</p>
 			</div>
