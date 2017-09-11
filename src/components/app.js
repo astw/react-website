474b85761c3fe9b8dd4a4f0c0e8b0b32ba85fb32
Shuhao Wang <wshuhao@gmail.com>
2017-09-10 22:09:11 -0700

    add react route

---
+++ src/components/app.js
@@ -0,0 +1,22 @@
+/*eslint-disable strict */
+//"use strict";
+
+var React = require("react");
+var Header = require('./common/Header');
+var RouteHandler = require("react-router").RouteHandler;
+$ = jQuery = require('jQuery');
+
+var App = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <Header />
+                <div className="container-fluid">
+                  <RouteHandler/>
+                </div>
+            </div>
+        );
+    }
+});
+
+module.exports = App;
