aec2b329f4484b1844d6b067509f68b4c0cb065e
Shuhao Wang <wshuhao@gmail.com>
2017-09-13 22:31:22 -0700

    willTransitionTo,  willTransitionFrom

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -1,11 +1,27 @@
-"use strict"; 
+"use strict";
 
-var React = require('react'); 
+var React = require('react');
 
 var About = React.createClass({
+	statics: {
+		willTransitionTo: function(transition, params,query, callback){
+			if(!confirm("Are you sure you want to read a page that'\s this boroing Page ")){
+				transition.abort();
+			} else {
+				callback();
+			}
+		},
+
+		willTransitionFrom: function(transition, component){
+			if(!confirm("Are you sure you want from read a page that'\s this existing Page ")){
+				transition.abort();
+			}  
+		}
+	},
+
 	render: function(){
-		return ( 
-			<div> 
+		return (
+			<div>
 				<h1>About</h1>
 				<p>
 					This application uses the following technologies:
