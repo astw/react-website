7d89da693b48cb8b92ee0f2193baf80f2ebabbf7
Shuhao Wang <wshuhao@gmail.com>
2017-09-13 22:41:32 -0700

    using html5 route stype

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -15,7 +15,7 @@ var Header = React.createClass({
 					<ul className="nav navbar-nav">
 						<li><Link to="app" > Home </Link></li>
 						<li><Link to="authors">Authors</Link></li>
-						<li><a href="/#about-us">About</a></li>
+						<li><a href="about-us">About</a></li>
 					</ul>
 				</div>
 			</nav>
